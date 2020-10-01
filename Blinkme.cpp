40c6fae5b7aa0ab0286b0812bc5168fdb54b938b
Prashantrasaily <71700231+Prashantrasaily@users.noreply.github.com>
2020-10-01 16:09:27 +0530

    Create Blinkme.cpp

---
+++ Blinkme.cpp
@@ -0,0 +1,22 @@
+const uint8_t btn_pin = 2; // unsigned integer (8 bits)  (0 to 255)
+const uint8_t led_pin = 5;
+
+uint8_t led_state = LOW;
+uint8_t btn_prev  = HIGH;
+
+void setup() {
+pinMode(btn_pin,INPUT_PULLUP);
+pinMode(led_pin,OUTPUT);
+attachInterrupt(digitalPinToInterrupt(btn_pin),toggle,FALLING);
+
+}
+
+void loop() {
+
+
+ delay(500);
+}
+void toggle(){
+  led_state = !led_state;
+  digitalWrite(led_pin,led_state);
+}
